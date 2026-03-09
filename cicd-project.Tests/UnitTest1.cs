7817853851161debe6df1f35adaf581a5c3ab062
Omeed Khalidi <omeedkhalidi0@gmail.com>
2026-03-09 20:22:45 +0100

    add unit tests

---
+++ cicd-project.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace cicd_project.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- obj/Debug/net9.0/cicd-project.AssemblyInfo.cs
+++ obj/Debug/net9.0/cicd-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cicd-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54434f491b154be71e7bb4d9c6be40382f606471")]
 [assembly: System.Reflection.AssemblyProductAttribute("cicd-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cicd-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
